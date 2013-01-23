80fe94fe17cafb99e61c7a210813d6e0ea7e668f
error454 <zachary.burke@gmail.com>
2013-01-22 21:45:47 -0800

    fix RS_Y

--- shiva/Resources/Scripts/OuyaController_Handler_onOuyaJoystick.lua
+++ shiva/Resources/Scripts/OuyaController_Handler_onOuyaJoystick.lua
@@ -14,7 +14,7 @@ function OuyaController.onOuyaJoystick ( nPlayer, ls_x, ls_y, rs_x, rs_y, l2, r2
     hset( hash, "AXIS_LS_X", ls_x )
     hset( hash, "AXIS_LS_Y", ls_y )
     hset( hash, "AXIS_RS_X", rs_x )
-    hset( hash, "AXIS_RS_Y", rs_x )
+    hset( hash, "AXIS_RS_Y", rs_y )
     hset( hash, "AXIS_L2", l2 )
     hset( hash, "AXIS_R2", r2 )
 	
