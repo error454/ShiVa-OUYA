03bd132fc3f51b5999a51ed7c9a0f40af403b4ef
error454 <zachary.burke@gmail.com>
2013-05-27 13:13:41 -0700

    fix variable reference

--- shiva/Resources/Scripts/GameAI_Function_getOuyaAxis.lua
+++ shiva/Resources/Scripts/GameAI_Function_getOuyaAxis.lua
@@ -8,7 +8,7 @@
 function GameAI.getOuyaAxis ( nPlayer, sAxis )
 --------------------------------------------------------------------------------
 	
-	user.sendEventImmediate ( this.getUser ( ), "OuyaController", "onGetAxis", nPlayer, sKey )
+	user.sendEventImmediate ( this.getUser ( ), "OuyaController", "onGetAxis", nPlayer, sAxis )
     return user.getAIVariable ( this.getUser ( ), "OuyaController", "nResult" )
 	
 --------------------------------------------------------------------------------
